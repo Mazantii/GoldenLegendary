4faaaf1e01247313706b13568da0dba44f1a1954
emzar <95346980+MilGaming@users.noreply.github.com>
2024-04-19 19:48:15 +0200

    stats for player :+1:

---
+++ Golden Legendary/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 100;
+    public int maxHealth = 100;
+    public int damage = 1;
+    public int fireRate = 1;
+    public int speed = 5;
+    public float dodgeRate = 0f;
+    public float CritRate = 0f;
+    public int CritDamage = 2;
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+
+    //run this function when the player takes damage
+    public void TakeDamage(int damage) //take damage from enemy
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            //Die code here
+        }
+    }
+
+/// <summary>
+/// function that increases the player's stats
+/// </summary>
+
+    public void HealAtWaveEnd(int heal) //heal at the end of the level
+    {
+        health = maxHealth;
+    }
+
+    public void IncreaseDamage(int damageIncrease) //increase damage
+    {
+        damage += damageIncrease;
+    }
+
+    public void IncreaseFireRate(int fireRateIncrease) //increase fire rate
+    {
+        fireRate += fireRateIncrease;
+    }
+
+    public void IncreaseSpeed(int speedIncrease) //increase speed
+    {
+        speed += speedIncrease;
+    }
+
+    public void IncreaseDodgeRate(float dodgeRateIncrease) //increase dodge rate
+    {
+        dodgeRate += dodgeRateIncrease;
+    }
+
+    public void IncreaseCritRate(float critRateIncrease) //increase crit rate
+    {
+        CritRate += critRateIncrease;
+    }
+
+    public void IncreaseCritDamage(int critDamageIncrease) //increase crit damage
+    {
+        CritDamage += critDamageIncrease;
+    }
+
+
+/// <summary>
+/// function that decreases the player's stats
+/// </summary>
+    
+    public void DecreaseDamage(int damageDecrease) //decrease damage
+    {
+        damage -= damageDecrease;
+    }
+
+    public void DecreaseFireRate(int fireRateDecrease) //decrease fire rate
+    {
+        fireRate -= fireRateDecrease;
+    }
+
+    public void DecreaseSpeed(int speedDecrease) //decrease speed
+    {
+        speed -= speedDecrease;
+    }
+
+    public void DecreaseDodgeRate(float dodgeRateDecrease) //decrease dodge rate
+    {
+        dodgeRate -= dodgeRateDecrease;
+    }
+
+    public void DecreaseCritRate(float critRateDecrease) //decrease crit rate
+    {
+        CritRate -= critRateDecrease;
+    }
+
+    public void DecreaseCritDamage(int critDamageDecrease) //decrease crit damage
+    {
+        CritDamage -= critDamageDecrease;
+    }
+
+
+/// <summary>
+/// function that resets the player's stats
+/// </summary>
+    public void ResetStats() //reset stats if died
+    {
+        health = maxHealth;
+        damage = 1;
+        fireRate = 1;
+        speed = 5;
+        dodgeRate = 0f;
+        CritRate = 0f;
+        CritDamage = 2;
+    }
+
+}
