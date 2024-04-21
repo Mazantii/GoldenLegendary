4555ba895abfd81daf837ca31d6482326150df8e
JonasFrellesen <jonasfpetersen@gmail.com>
2024-04-21 11:07:20 +0200

    animations for creatures

--- Golden Legendary/Assets/Scripts/Player Movement.cs
+++ Golden Legendary/Assets/Scripts/Player Movement.cs
@@ -8,14 +8,18 @@ public class PlayerMovement : MonoBehaviour
     PlayerInput playerInput;
     InputAction movement;
 
+    //private Animator anim;
+
 
     //player stats
     PlayerStats playerStats;
 
     void Start()
     {
+        
         playerInput = GetComponent<PlayerInput>();
         movement = playerInput.actions["Move"];
+        //anim = GetComponent<Animator>();
 
         //Find the script with the player Stats
         playerStats = GetComponent<PlayerStats>();
@@ -30,6 +34,15 @@ public class PlayerMovement : MonoBehaviour
     void MovePLayer()
     {
         Vector2 move = movement.ReadValue<Vector2>();
+
+        /*if(move = movement.ReadValue<Vector2>("A"))
+        {
+            anim.SetBool("isAttackingLeft", true);
+        }
+        else if(move = movement.ReadValue<Vector2>("D"))
+        {
+            anim.SetBool("isAttackingRight", true);
+        }*/
 
         transform.position += new Vector3(move.x, move.y, 0) * playerStats.speed * Time.deltaTime;
     }
